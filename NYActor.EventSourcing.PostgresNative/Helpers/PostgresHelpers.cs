4ed67f2c821a185e83ca6b20cceb0b9d3fe4bb0b
Roman Barinov <rbarinov@gmail.com>
2023-01-03 18:16:30 +0400

    done binary import for esnpgsql

---
+++ NYActor.EventSourcing.PostgresNative/Helpers/PostgresHelpers.cs
@@ -0,0 +1,31 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace NYActor.EventSourcing.PostgresqlNative.Helpers;
+
+public static class PostgresHelpers
+{
+    public static Task WriteSafeAsync<T>(
+        this NpgsqlBinaryImporter writer,
+        T? value,
+        CancellationToken cancellationToken = default
+    )
+        where T : struct
+        => value.HasValue
+            ? writer.WriteAsync(value.Value, cancellationToken)
+            : writer.WriteNullAsync(cancellationToken);
+
+    public static Task WriteSafeAsync<T>(
+        this NpgsqlBinaryImporter writer,
+        T value,
+        CancellationToken cancellationToken = default
+    )
+        => writer.WriteAsync(value, cancellationToken);
+
+    public static Task WriteArraySafeAsync(
+        this NpgsqlBinaryImporter writer,
+        List<string> value,
+        CancellationToken cancellationToken = default
+    )
+        => writer.WriteAsync(value, NpgsqlDbType.Array | NpgsqlDbType.Text, cancellationToken);
+}
